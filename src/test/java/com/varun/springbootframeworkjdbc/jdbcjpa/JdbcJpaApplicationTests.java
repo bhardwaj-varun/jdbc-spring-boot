39a035c6f9deac1d51b2c92e9b12077e76a696a3
Varun Bhardwaj <varunbhardwaj@tutanota.com>
2020-02-03 10:56:43 +0530

    jdbc implementation using spring boot

---
+++ src/test/java/com/varun/springbootframeworkjdbc/jdbcjpa/JdbcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.springbootframeworkjdbc.jdbcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
